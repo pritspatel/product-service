50e0cef4b8820efc2363642a78f6a44f96db1790
Pritesh Patel <3ppatel@gmail.com>
2020-04-12 15:28:29 -0400

    Initial import

---
+++ src/main/java/com/prits/config/DevCorsConfig.java
@@ -0,0 +1,19 @@
+package com.prits.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class DevCorsConfig implements WebMvcConfigurer {
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .exposedHeaders("Authorization");
+    }
+}
---
+++ src/main/java/com/prits/web/ProductController.java
@@ -0,0 +1,15 @@
+package com.prits.web;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class ProductController {
+
+    @GetMapping("/product")
+    public Product getProducts(){
+        return new Product("iPad","A table with 10 inch screen from Apple");
+    }
+}
