09b53ac553677ee76e90dddae56fa04c09a46c0b
Pritesh Patel <3ppatel@gmail.com>
2020-04-13 13:47:55 -0400

    Initial Import

--- src/main/java/com/prits/web/ProductController.java
+++ src/main/java/com/prits/web/ProductController.java
@@ -1,14 +1,16 @@
 package com.prits.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     @GetMapping("/product")
+    //@PreAuthorize("hasRole('READ')")
     public Product getProducts(){
         return new Product("iPad","A table with 10 inch screen from Apple");
     }
